ed03bdcf50a103c93310ebd3e1df5c0dca80637d
Jubayer Ahmed <itsjubayer@gmail.com>
2024-08-30 19:33:13 +0600

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using EmployeePortal.Models;
+using EmployeePortal.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeRepository emp;
+
+        public EmployeeController(EmployeeRepository empRepository)
+        {
+            this.emp = empRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EmployeeList()
+        {
+            var allEmployees = await emp.GetAllEmployees();
+            return Ok(allEmployees);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddEmployee(Employee vm)
+        {
+            await emp.SaveEmployee(vm);
+            return Ok(vm);
+        }
+
+
+
+
+    }
+}
